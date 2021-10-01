c45fd4f9e8031e1f64210d4b27e2b1d55c4035cf
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2021-10-01 14:00:30 +0900

    First version

---
+++ ytdlgui/resource.h
@@ -0,0 +1,29 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ で生成されたインクルード ファイル。
+// ytdlgui.rc で使用
+//
+#define IDC_MYICON                      2
+#define IDD_YTDLGUI_DIALOG              102
+#define IDD_MAIN                        102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_YTDLGUI                     107
+#define IDI_SMALL                       108
+#define IDC_YTDLGUI                     109
+#define IDR_MAINFRAME                   128
+#define IDC_STATIC                      -1
+
+#define IDC_URL 1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
---
+++ ytdlgui/ytdlgui.c
@@ -0,0 +1,77 @@
+﻿#include "framework.h"
+#include "ytdlgui.h"
+
+#define BUFFERSIZE 1024
+
+HINSTANCE hMainInst;
+int YouTubeDownload(HWND hDlg);
+INT_PTR CALLBACK MainDlg(HWND hDlg, UINT msg, WPARAM wPrm, LPARAM lPrm);
+
+int APIENTRY _tWinMain(HINSTANCE hInst, HINSTANCE hPInst, LPTSTR lpCmdLine, int nCmdShow)
+{
+	hMainInst = hInst;
+	return DialogBox(hInst, MAKEINTRESOURCE(IDD_MAIN), NULL, MainDlg);
+}
+
+INT_PTR CALLBACK MainWin(HWND hDlg, UINT msg, WPARAM wPrm, LPARAM lPrm)
+{
+	UNREFERENCED_PARAMETER(lPrm);
+
+	switch (msg) {
+
+	}
+
+	return (INT_PTR)FALSE;
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK MainDlg(HWND hDlg, UINT msg, WPARAM wPrm, LPARAM lPrm)
+{
+	switch (msg) {
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wPrm)) {
+		case IDOK:
+			YouTubeDownload(hDlg);
+			break;
+		case IDCANCEL:
+			EndDialog(hDlg, LOWORD(wPrm));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+
+	return (INT_PTR)FALSE;
+}
+
+int YouTubeDownload(HWND hDlg)
+{
+	TCHAR buf[BUFFERSIZE];
+	TCHAR cmd[BUFFERSIZE];
+
+	GetDlgItemText(hDlg, IDC_URL, buf, BUFFERSIZE);
+	StringCchPrintf(cmd, BUFFERSIZE, _T("youtube-dl \"%s\""), buf);
+	_tsystem(cmd);
+	MessageBox(hDlg, _T("動画のダウンロードが完了しました"), _T("YouTube DL GUI"), 0);
+
+	return 0;
+}
